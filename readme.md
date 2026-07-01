6ed7f48c24c640bf6b2cb76762afd246e372b239
2784502 <2784502@tcs.com>
2026-07-01 15:52:01 +0530

    Initial repository setup

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Sample Terraform Repository
+
+Repository used for automation testing.
---
+++ terraform/main.tf
@@ -0,0 +1,4 @@
+resource "azure_function" "function_app" {
+  name     = "sample-function-app"
+  location = "East US"
+}
