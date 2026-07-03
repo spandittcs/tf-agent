20c26a37d3d51bf87633f2f743242f183b5e55ad
2784502 <2784502@tcs.com>
2026-07-03 16:46:57 +0530

    Agent automated repository update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-resource "azure_function_app" "function_app" {
+resource "azure_windows_function_app" "function_app" {
 
   name                = var.function_name
   location            = var.location
